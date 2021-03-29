14d4149667d8096ab8950c5594d5ccb662371764
Vlad <vladprodan6@gmail.com>
2021-03-29 22:44:51 +0300

    card event and eventId logic

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,6 +1,20 @@
-import React from 'react'
+import {useRouter} from 'next/router'
+import { getEventById } from '../../dummy-data';
+
 
 function EventDetailPage() {
+    const router = useRouter()
+    
+    const eventId = router.query.eventId;
+    const event = getEventById(eventId)
+
+    if(!event) {
+        return (
+            <p>No event</p>
+        )
+    }
+
+
     return (
         <div>
             <h1>Event Detail</h1>
